49d2ac9e7cbc641e4f9ee7de0ec79f64a9dda694
William Ricardo Torres Curtidor <87676762+wtorre5@users.noreply.github.com>
2021-09-17 19:36:52 -0500

    add model articles

---
+++ lib/Models/articles.model.dart
@@ -0,0 +1,21 @@
+class Article{
+
+  String author;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
+  String publishedAt;
+  String context;
+
+  Article({
+    required this.author,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.urlToImage,
+    required this.publishedAt,
+    required this.context,
+  });
+
+}
