e012f5d9177dfc2db9822f94405e191f80d3a2d7
William Ricardo Torres Curtidor <87676762+wtorre5@users.noreply.github.com>
2021-09-17 20:40:09 -0500

    Fetch Articles from newsAPI

--- lib/Models/articles.model.dart
+++ lib/Models/articles.model.dart
@@ -18,4 +18,14 @@ class Article{
     required this.context,
   });
 
+  factory Article.fromJson(Map<String, dynamic> json) => Article(
+    author : json["author"] ?? 'Not Author',
+    title : json["title"] ?? 'Not Title',
+    description : json["description"] ?? 'Not Description',
+    url : json["url"] ?? 'Not URL',
+    urlToImage: json["urlToImage"] ?? 'Not URL To Image',
+    publishedAt : json["published_at"] ?? 'Not Published At',
+    context : json["context"] ?? 'Not Context',
+  );
+
 }
--- lib/Provider/articles.provider.dart
+++ lib/Provider/articles.provider.dart
@@ -1,19 +1,27 @@
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
+import 'package:semana3noticias/Models/articles.model.dart';
 
 class ArticleProvider {
+  Future<List<Article>> getArticles() async {
+    List<Article> articleList = [];
+    var url = Uri.https(
+      'newsapi.org',
+      '/v2/everything',
+      {
+        'q': 'tesla',
+        'from': '2021-09-15',
+        'sortBy': 'publishedAt',
+        'apiKey': 'e8f01d6b811144ca8e90b630baa05224'
+      },
+    );
 
-  fetchArticles() async {
-    var url = Uri.https('www.googleapis.com', '/books/v1/volumes', {'q': '{http}'});
     // Await the http get response, then decode the json-formatted response.
     var response = await http.get(url);
     if (response.statusCode == 200) {
-      var jsonResponse =
-          convert.jsonDecode(response.body) as Map<String, dynamic>;
-      var itemCount = jsonResponse['totalItems'];
-      print('Number of books about http: $itemCount.');
-    } else {
-      print('Request failed with status: ${response.statusCode}.');
+      var jsonResponse = convert.jsonDecode(response.body) as Map<String, dynamic>;
+      jsonResponse['articles'].forEach((item) => articleList.add(Article.fromJson(item)));
     }
+    return articleList;
   }
 }
